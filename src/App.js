8116e64083bd596f7dbc5936c6816f1355a315e7
Batool Mahmoud <batool.alnaib@gmail.com>
2025-02-06 15:07:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import './App.css';
+
+const RoundedBoxes = () => {
+  return (
+    <div className="container">
+      <div className="box">South Asia</div>
+      <div className="box">East Asia</div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Xenial Xerus</h1>
+      <header>
+        <h1>SNPs associated with Type 2 diabetes in Asian Populations</h1>
+        <h3>Xenial Xerus</h3>
+      </header>
+
       <p>
         This website is an SNP browser website dedicated to highlighting SNPs associated with Type 2 diabetes in populations understudied, specifically Southern and Eastern Asia.
         This website includes gene ontology, SNP location, p-values for such associations as well as a number of summary statistics.
       </p>
-      
-      {/* Search Bar */}
+
       <input
-        type = "text"
-        placeholder='Search...'
-        style={{
-          padding: '10px',
-          marginTop: '20px',
-          fontSize: '16px',
-          width: '75%',
-          maxWidth: '300px',
-        }}/>
+        type="text"
+        placeholder="Search..."
+        className="search-bar"
+      />
+
+      <RoundedBoxes />
+
+      
+      <h2 className="section-heading">Graph Showing Type 2 Diabetes Prevalence in Different Ethnicities in the UK</h2>
+
+      <section className="research-section">
+        <div className='graph'>
+          <img
+            src='https://onlinelibrary.wiley.com/cms/asset/06834406-c898-4c6b-9151-e1e7418b1ccb/dme13895-fig-0002-m.jpg'
+            alt='SNP Graph'
+          />
+        </div>
+
+        <div className="description">
+          <p>
+              PLACEHOLDER
+          </p>
+        </div>
+      </section>
+
+      <footer>
+        <p>© 2025 Xenial Xerus. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
