80747a657ca323ab2c2a517daa11b80bb1302f69
Batool Mahmoud <batool.alnaib@gmail.com>
2025-02-20 15:55:38 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,10 @@
+
 import sqlite3
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def welcome():
@@ -20,23 +23,40 @@ def np():
         cursor = conn.cursor()
 
         print(f"Executing query to search for SNP: {search_query}")
-        cursor.execute("SELECT * FROM SNPS WHERE rs_id LIKE ?", ('%' + search_query + '%',))
+        query = """
+            SELECT
+                SNPS.rs_id,
+                GENES.gene_symbol
+                GENES.start_pos
+                GENES.end_pos 
+            FROM SNPS 
+            JOIN GENES ON SNPS.gene_id = GENES.id
+            WHERE SNPS.rs_id LIKE ?
+            """ 
+        cursor.execute(query,('%' + search_query + '%',))
         rows = cursor.fetchall()
-        print(f"Rows found: {rows}")
+        
         conn.close()
+        
+        if not rows:
+            return jsonify({"NO SNPS FOUND"})
+
+        snps = []
+        for row in rows:
+            snps.append({
+                'rs_id': row[0],
+                'Gene': row[1],
+                'Start Position':row[2],
+                'End Position': row[3]
+
+            })
+
+        return jsonify(snps)
+
     except Exception as e:
-        print(f"Error occurred: {str(e)}")
+
+        print(f"Error: {str(e)}")
         return jsonify({"error": str(e)}), 500
-
-    snps = []
-    for row in rows:
-        snps.append({
-            'id': row[0],
-            'gene': row[1]
-        })
-
-    return jsonify(snps)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
