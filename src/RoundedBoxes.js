1f621781c0b44cfdfad2f4acc0bcc2b57bac79e4
Batool Mahmoud <batool.alnaib@gmail.com>
2025-02-22 14:29:09 +0000

    Update RoundedBoxes.js

--- src/RoundedBoxes.js
+++ src/RoundedBoxes.js
@@ -1,13 +1,14 @@
-
+//import the libraries
 import React from 'react';
 import { Link } from 'react-router-dom';  
 
+//defines functional component which renders links called South Asia and East Asia that links to the east and south asia js pages
 const RoundedBoxes = () => {
   return (
     <div className="container">
      
-      <Link to="/south-asia" className="box">South Asia</Link>
-      <Link to="/east-asia" className="box">East Asia</Link>
+      <Link to="/south-asia" className="box">SouthAsia</Link>
+      <Link to="/east-asia" className="box">EastAsia</Link>
     </div>
   );
 };
