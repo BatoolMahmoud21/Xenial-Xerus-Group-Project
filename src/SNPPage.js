0850f6b1652f8d9ad9dadc343f21c46c99def25a
Batool Mahmoud <batool.alnaib@gmail.com>
2025-02-25 14:07:01 +0000

    Update Links SNPPage.js

--- src/SNPPage.js
+++ src/SNPPage.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';  
+import { Link } from 'react-router-dom';
 
-// Creates a function
 function SNPPage() {
-  // Access the search results from location state
   const { state } = useLocation();
   const { searchResults } = state || {};  // Extract searchResults from state
 
   return (
     <div className="SNPPage">
       <h1>SNP Search Results</h1>
-
-      {/* Display search results in SNPPage */}
       {searchResults && searchResults.length > 0 ? (
         <table border="1" cellpadding="5" cellspacing="0" style={{ borderCollapse: 'collapse' }}>
           <thead>
             <tr>
-              {/* Column Head */}
               <th>rs ID</th>
               <th>Gene</th>
               <th>Gene Name</th>
@@ -25,6 +20,7 @@ function SNPPage() {
               <th>Start Position</th>
               <th>End Position</th>
               <th>p Value</th>
+              <th>Population</th>
             </tr>
           </thead>
           <tbody>
@@ -39,6 +35,10 @@ function SNPPage() {
                 <td>{snp['Start Position']}</td>
                 <td>{snp['End Position']}</td>
                 <td>{snp['p Value']}</td>
+                <td>
+                  {/* Link with both Population and rs_id */}
+                  <Link to={`/population/${snp.Population}/${snp.rs_id}`}>{snp.Population}</Link>
+                </td>
               </tr>
             ))}
           </tbody>
